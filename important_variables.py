4355fb4d2c67845cb912751acf569e720fbce650
Ankush Pandey <ap539813@gmail.com>
2022-07-05 12:48:37 +0530

    Updated screen height and width

--- important_variables.py
+++ important_variables.py
@@ -1,6 +1,6 @@
 import pygame
 
-screen_width = 900
-screen_height = 900
+screen_width = 700
+screen_height = 700
 gameWindow = pygame.display.set_mode((screen_width, screen_height))
 green = (1, 50, 32)
