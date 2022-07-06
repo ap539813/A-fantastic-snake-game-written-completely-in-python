a42a4cc031088ad8ef4e621b078824dc3e72f347
Ankush Pandey <ap539813@gmail.com>
2022-07-06 19:59:50 +0530

    scales the apple image according to the requirement

--- assets.py
+++ assets.py
@@ -1,4 +1,7 @@
 import pygame
+from important_variables import apple_width
+
 carImg = pygame.image.load('apple.png')
+carImg = pygame. transform.scale(carImg, (apple_width, apple_width))
 overImg = pygame.image.load('tenor.gif')
 welcm = pygame.image.load('welcome_page.png')
