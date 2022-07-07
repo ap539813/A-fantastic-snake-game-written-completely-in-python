4a5da97927515a5cb28a6ec26e9a16c7f16f6d44
Ankush Pandey <ap539813@gmail.com>
2022-07-07 13:19:08 +0530

    Updated the path of assets

--- assets.py
+++ assets.py
@@ -1,10 +1,11 @@
 import pygame
 from important_variables import apple_width, screen_width, screen_height
+from paths import apple_path, game_over_image_path, welcome_page_image_path
 
-carImg = pygame.image.load('asset/apple.png')
-carImg = pygame. transform.scale(carImg, (apple_width, apple_width))
-overImg = pygame.image.load('asset/tenor.gif')
-welcm = pygame.image.load('asset/welcome_page.png')
+appleImg = pygame.image.load(apple_path)
+appleImg = pygame. transform.scale(appleImg, (apple_width, apple_width))
+overImg = pygame.image.load(game_over_image_path)
+welcm = pygame.image.load(welcome_page_image_path)
 welcm = pygame. transform.scale(welcm, (screen_width, screen_height))
 
 
