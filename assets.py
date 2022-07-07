1fec458517cad5734486cf7542129005bf8ef384
Ankush Pandey <ap539813@gmail.com>
2022-07-07 12:32:31 +0530

    Refactored the structure of the assets

--- assets.py
+++ assets.py
@@ -1,8 +1,8 @@
 import pygame
 from important_variables import apple_width, screen_width, screen_height
 
-carImg = pygame.image.load('apple.png')
+carImg = pygame.image.load('asset/apple.png')
 carImg = pygame. transform.scale(carImg, (apple_width, apple_width))
-overImg = pygame.image.load('tenor.gif')
-welcm = pygame.image.load('welcome_page.png')
+overImg = pygame.image.load('asset/tenor.gif')
+welcm = pygame.image.load('asset/welcome_page.png')
 welcm = pygame. transform.scale(welcm, (screen_width, screen_height))
