ab4e36886f06d4f0604b9ac099e26632d6dcd788
Ankush Pandey <ap539813@gmail.com>
2022-07-07 13:35:40 +0530

    Adde the new resized apple file

--- paths.py
+++ paths.py
@@ -3,6 +3,6 @@ bite_sound = 'asset/bite.wav'
 score_file = 'score/high_score.txt'
 game_over_sound = 'asset/over.mp3'
 
-apple_path = 'asset/apple_2.png'
+apple_path = 'asset/apple_2-2.png'
 game_over_image_path = 'asset/tenor.gif'
 welcome_page_image_path = 'asset/welcome_page.png'
