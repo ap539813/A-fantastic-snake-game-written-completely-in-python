0bc66f8b224c61296b7d593bb51331fbb843fbe8
Ankush Pandey <ap539813@gmail.com>
2022-07-07 13:18:45 +0530

    created module for paths

---
+++ paths.py
@@ -0,0 +1,8 @@
+background_sound = 'asset/media.io_background.wav'
+bite_sound = 'asset/bite.wav'
+score_file = 'score/high_score.txt'
+game_over_sound = 'asset/over.mp3'
+
+apple_path = 'asset/apple_2.png'
+game_over_image_path = 'asset/tenor.gif'
+welcome_page_image_path = 'asset/welcome_page.png'
