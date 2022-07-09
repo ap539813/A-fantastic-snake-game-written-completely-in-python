02e59264822bcd621a87e384a8e19ff0a4872d05
Ankush Pandey <ap539813@gmail.com>
2022-07-09 23:20:26 +0530

    uploaded path to new snakehead image

--- paths.py
+++ paths.py
@@ -4,10 +4,10 @@ score_file = 'score/high_score.txt'
 game_over_sound = 'asset/over.mp3'
 
 apple_path = 'asset/apple_2-2.png'
-game_over_image_path = 'asset/tenor.gif'
+game_over_image_path = 'asset/Game_over.png'
 welcome_page_image_path = 'asset/welcome_page.png'
 
-game_background = 'asset/background_image_3.png'
+game_background = 'asset/background_2.png'
 
-snake_head = 'asset/snake_head_2.png'
+snake_head = 'asset/snake_head_3.png'
 score_board = 'asset/ground_color.png'
