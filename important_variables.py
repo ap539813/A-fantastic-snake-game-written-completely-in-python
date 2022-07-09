974ec5012c9265534c3c81463b6264a2dccb052f
Ankush Pandey <ap539813@gmail.com>
2022-07-09 23:53:08 +0530

    Update important_variables.py

--- important_variables.py
+++ important_variables.py
@@ -4,11 +4,11 @@ import pygame
 screen_width = 700
 screen_height = 700
 gameWindow = pygame.display.set_mode((screen_width, screen_height))
-snake_colour = (140,198,62)# (1, 50, 32)
+snake_colour = (13,125,106)# light_green(140,198,62)# (1, 50, 32)
 eye_color = (0, 0, 0)
 score_board_size = 60
 apple_width = 40
 snake_width = 23 #screen_width//30
 
 
-text_colour = (139,69,19) #(139,69,19)
+text_colour = (140,198,62) #(139,69,19)
