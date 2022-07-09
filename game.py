0cc112111b5c6f299fe475a23a4bffe7bb92b5a5
Ankush Pandey <ap539813@gmail.com>
2022-07-09 09:51:40 +0530

    Update in the game font etc

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 
-from important_variables import screen_width, screen_height, score_board_size, apple_width
+from important_variables import screen_width, screen_height, score_board_size, apple_width, snake_width
 from paths import background_sound
 
 def start_game():
@@ -11,13 +11,12 @@ def start_game():
     game_over = False
     snake_x = screen_width//9
     snake_y = screen_height//9
-    snake_width = screen_width//45
     FPS = 60
     snake_mv = 'r'
     food_pos = (random.randint(1, screen_width-apple_width), random.randint(score_board_size + 10, screen_height-apple_width))
     head = [[snake_x, snake_y]]
     snake_len = 1
     clock = pygame.time.Clock()
-    font = pygame.font.SysFont("Snake Chan", score_board_size//2)
+    font = pygame.font.SysFont("Snake Chan", score_board_size//3)
     font2 = pygame.font.SysFont("Snake Chan", score_board_size*3//4)
     return exit_game, game_over, snake_x, snake_y, snake_width, FPS, snake_mv, food_pos, head, snake_len, clock, font, font2
