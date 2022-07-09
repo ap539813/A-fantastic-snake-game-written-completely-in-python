1885f99554dee56ffa91e6ac72dc962c02c867f7
Ankush Pandey <ap539813@gmail.com>
2022-07-09 09:50:44 +0530

    set the color, snake_width and apple_width

--- important_variables.py
+++ important_variables.py
@@ -1,8 +1,11 @@
+from turtle import color
 import pygame
 
 screen_width = 700
 screen_height = 700
 gameWindow = pygame.display.set_mode((screen_width, screen_height))
-green = (1, 50, 32)
+colour = (30,144,255)#(1, 50, 32)
+eye_color = (0, 0, 0)
 score_board_size = 60
 apple_width = 40
+snake_width = screen_width//30
