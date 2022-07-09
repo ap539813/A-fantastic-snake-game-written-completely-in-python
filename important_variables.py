fdd60c0ede44234c9aeaeee942fd4c1ad969ced1
Ankush Pandey <ap539813@gmail.com>
2022-07-09 19:30:20 +0530

    defined color and width for games elements

--- important_variables.py
+++ important_variables.py
@@ -1,11 +1,14 @@
-from turtle import color
+# from turtle import color
 import pygame
 
 screen_width = 700
 screen_height = 700
 gameWindow = pygame.display.set_mode((screen_width, screen_height))
-colour = (30,144,255)#(1, 50, 32)
+snake_colour = (140,198,62)# (1, 50, 32)
 eye_color = (0, 0, 0)
 score_board_size = 60
 apple_width = 40
-snake_width = screen_width//30
+snake_width = 23 #screen_width//30
+
+
+text_colour = (139,69,19) #(139,69,19)
